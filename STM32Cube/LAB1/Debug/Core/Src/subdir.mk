eb0c58501339615fc8ebbd75452d8b193c3a093c
THANH VƯƠNG <thisun34@gmail.com>
2025-09-19 00:03:05 +0700

    no message

--- STM32Cube/LAB1/Debug/Core/Src/subdir.mk
+++ STM32Cube/LAB1/Debug/Core/Src/subdir.mk
@@ -5,7 +5,6 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/Src/ex3.c \
 ../Core/Src/ex4.c \
 ../Core/Src/main.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
@@ -15,7 +14,6 @@ C_SRCS += \
 ../Core/Src/system_stm32f1xx.c 
 
 OBJS += \
-./Core/Src/ex3.o \
 ./Core/Src/ex4.o \
 ./Core/Src/main.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
@@ -25,7 +23,6 @@ OBJS += \
 ./Core/Src/system_stm32f1xx.o 
 
 C_DEPS += \
-./Core/Src/ex3.d \
 ./Core/Src/ex4.d \
 ./Core/Src/main.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
