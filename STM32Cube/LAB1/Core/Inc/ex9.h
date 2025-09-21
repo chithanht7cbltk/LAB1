9e0bfe9e4a1b58a836be94c6bd3ec763c1b860fe
THANH VƯƠNG <thisun34@gmail.com>
2025-09-21 11:49:24 +0700

    no message

--- STM32Cube/LAB1/Core/Inc/ex9.h
+++ STM32Cube/LAB1/Core/Inc/ex9.h
@@ -10,8 +10,8 @@
 
 #include "main.h"
 
-void init_ex6();
-void ex6_run();
+void init_ex9();
+void ex9_run();
 
 
 #endif /* INC_EX6_H_ */
---
+++ STM32Cube/LAB1/Core/Src/ex9.c
@@ -0,0 +1,45 @@
+/*
+ * ex6.c
+ *
+ *  Created on: Sep 21, 2025
+ *      Author: cthanh
+ */
+
+
+#include "ex9.h"
+
+int counter = 0;
+
+void init_ex9(){}
+void clearAllClock();
+void clearNumberOnClock(int num);
+
+
+uint16_t LED[12] =
+{
+	GPIO_PIN_4,
+	GPIO_PIN_5,
+	GPIO_PIN_6,
+	GPIO_PIN_7,
+	GPIO_PIN_8,
+	GPIO_PIN_9,
+	GPIO_PIN_10,
+	GPIO_PIN_11,
+	GPIO_PIN_12,
+	GPIO_PIN_13,
+	GPIO_PIN_14,
+	GPIO_PIN_15
+
+};
+
+
+void clearNumberOnClock(int num)
+{
+	HAL_GPIO_WritePin(GPIOA, LED[num], GPIO_PIN_SET);
+}
+
+void ex9_run()
+{
+
+}
+
