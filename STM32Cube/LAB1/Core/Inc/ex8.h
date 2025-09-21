a6b75b05f69ea062a1f618337d4f68b5ea58d8b0
THANH VƯƠNG <thisun34@gmail.com>
2025-09-21 11:44:17 +0700

    no message

---
+++ STM32Cube/LAB1/Core/Inc/ex8.h
@@ -0,0 +1,17 @@
+/*
+ * ex8.h
+ *
+ *  Created on: Sep 21, 2025
+ *      Author: cthanh
+ */
+
+#ifndef INC_EX8_H_
+#define INC_EX8_H_
+
+#include "main.h"
+
+void init_ex8();
+void ex8_run();
+
+
+#endif /* INC_EX8_H_ */
--- STM32Cube/LAB1/Core/Src/main.c
+++ STM32Cube/LAB1/Core/Src/main.c
@@ -23,7 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
-#include"ex6.h"
+#include"ex8.h"
 
 /* USER CODE END Includes */
 
@@ -96,7 +96,7 @@ int main(void)
 
   while (1)
   {
-	  ex6_run();
+	  ex8_run();
 
 	  HAL_Delay(1000);
 
